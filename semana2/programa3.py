50ab2b08b0fc116c4fbc749105d5d483fc1bc29d
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-01-26 19:55:15 +0000

    se creo el programa4.py

--- semana2/programa3.py
+++ semana2/programa3.py
@@ -2,7 +2,7 @@
   Nombre: Johan Obed MM
   Fecha: 24/01/2023
   Descripción:
-"""
+""""
 numero1 =10
 numero2=5
 print(numero1+numero2)
---
+++ semana2/programa4.py
@@ -0,0 +1,13 @@
+""""
+  Nombre: Johan Obed MM
+  Fecha: 25/01/2023
+  Descripción:
+"""""
+
+numero1=10
+numero2=5
+print("{}+{}={}".format(numero1,numero2,numero1+numero2))
+print("{n1}+{n2}={suma}".format(n1=numero1,n2=numero2,suma=numero1+numero2))
+print("{n2}+{n2}={n2}".format(n1=numero1,n2=numero2,suma=numero1+numero2))
+print("{numero1}+{numero2}={suma}".format(numero1=numero1,numero2=numero2,suma=numero1+numero2))
+print("{}+{}={}".format(numero1,numero2,numero1+numero2))
