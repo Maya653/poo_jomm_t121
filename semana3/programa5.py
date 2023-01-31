af82acab4105da87b40bb62961b935f77032f04c
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-01-31 01:49:53 +0000

    se realizo suma resta multiplicación y potencia

---
+++ semana3/programa5.py
@@ -0,0 +1,33 @@
+""""
+  Nombre: Johan Obed MM
+  Fecha: 24/01/2023
+  Descripción:
+"""""
+numero1= input("Numero1: ")
+numero2 =10
+suma= int (numero1)+ numero2
+print(suma)
+
+num1 = int(input("Ingrese el primer número: "))
+num2 =10
+result = num1 + num2
+print("La suma de", num1, "y", num2, "es", result)
+
+num1= int(input("ingrese el primer numero: "))
+num2=20
+result= num1-num2
+print("la resta de",num1,"y", num2, "es", result )
+
+num1=int(input ("ingrese el numero: "))
+num2=5
+result=num1*num2
+print("la multiplicacion de",num1,"y",num2,"es", result)
+
+
+numero1 = int (input("Ingrese numero1: "))
+numero2 = 2
+result=numero1**numero2
+print("la potencia de ",numero1,"y", numero2,"es",result)
+    
+
+
