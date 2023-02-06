5d38e58557a0cf24a5ff8028c2ceee184dafeb59
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-02-06 00:52:36 +0000

    para empezar (5 de febrero del 2023)

---
+++ reto/domingo5.py
@@ -0,0 +1,13 @@
+""""
+    Nombre: Johan Obed MAya Morales
+    Fecha: 5-Feb-2023
+    Descripción: volviendo hacer hola mundo"
+"""""
+
+variable1="hola"
+variable2= "que hace"
+variable3= "hoy"
+variable4="5"
+variable5="de febrero"
+print(variable1 + variable2 + variable3 + variable4 + variable5)
+print("hola que hace hoy 5 de febrero")
