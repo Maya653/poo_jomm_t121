f3140c750d136d68990a138e46e0a92315512353
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-02-07 04:46:43 +0000

    hice la multiplicación de un numero cualquiera una variable con numero establecido, *todavía me guie del problema que se hizo en clase

---
+++ reto/lunes6.py
@@ -0,0 +1,9 @@
+""""
+    Nombre:johan Obed Maya Morales
+    Fecha:6-Feb-2023
+    Descripción: La multiplicación de un numero
+""""" # se me olvida siempre que lo creo son 5 comillas
+numero1= int(input("Ingrese un numero cualquiera: "))  # es poner primero la variable, despues de que tipo es en este caso es int (entero),despues input este me permite poner datos de entra 
+numero2= 2 # cree una variable y con un valor defenido
+multiplicacion= int (numero1*numero2) #cree una variable que me ahaga la operación de numero1 y numero2, que sean enteros INT y la multiplicacion de ambas variables anteriores
+print ("la multiplicacion de: ",numero1, "y", numero2, "es: ", multiplicacion) #por ultimo imprimo osea que lo que se va haber en SHell, le agregue un texto seguido de las variables, (NOTA para mi: llevan comas en cada terminacion sino me marca error y ando batallando )
