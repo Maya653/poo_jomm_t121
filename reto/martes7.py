877ad44af87b278a39083f1b157950ae5822be72
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-02-08 04:48:45 +0000

    trabajo que se realizo en clase

---
+++ reto/martes7.py
@@ -0,0 +1,14 @@
+""""
+    Nombre: Johan Obed Maya Morales
+    Fecha: 7-Feb-2023
+    Descripción: diferentes formas de crear el programa8.py
+"""""
+numero1=int(input("ingresa el valor1: "))
+numero2=int(input("ingresa el valor2:"))
+#SOLUCCIÓN 1
+if numero1 > numero2:
+    print(numero1)
+if numero2 > numero1:
+    print(numero2)
+if numero1 == numero2:
+    print(None)
---
+++ semana4/programa9.py
@@ -0,0 +1,30 @@
+""""
+    Nombre: Johan Obed Maya Morales
+    Fecha: 7-Feb-2023
+    Descripción: diferentes formas de crear el programa8.py
+"""""
+numero1=int(input("ingresa el valor1: "))
+numero2=int(input("ingresa el valor2:"))
+#SOLUCCIÓN 1
+if numero1 > numero2:
+    print(numero1)
+if numero2 > numero1:
+    print(numero2)
+if numero1 == numero2:
+    print(None)
+#SOLUCCIÓN 2
+if numero2 > numero1:
+    print(numero2)
+if numero1 > numero2:
+    print(numero1)
+else:
+    print(None)
+    #Solucción 3:
+if numero1 > numero2:
+    print(numero1)
+elif numero2 > numero1:
+    print(numero2)
+else:
+    print(None)
+    
+    
