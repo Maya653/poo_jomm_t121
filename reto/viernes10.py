d9db1610b217454b4713e459f9e359bd3befb546
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-02-11 03:55:42 +0000

    mi nombre y edad

---
+++ reto/viernes10.py
@@ -0,0 +1,11 @@
+""""
+    Nombre: Johan Obed Maya Morales
+    Fecha:9-Feb-2023
+    Descripción:
+""""" 
+
+nombre = "Johan"
+edad = 21
+
+print("Mi nombre es", nombre)
+print("Tengo", edad, "años")
