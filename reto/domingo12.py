a6cf78aea2ef4111c03aeb5a10f711e088fb0224
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-02-13 05:06:54 +0000

    hice de nuevo el triangulo,  el dato ya lo puse como comentario pa no ser mucho texto

---
+++ reto/domingo12.py
@@ -0,0 +1,20 @@
+""""
+    Nombre: Johan Obed MAya Morales
+    Fecha: 5-Feb-2023
+    Descripción: volviendo hacer el perimetro y area de u triangulo"
+"""""
+print("calcula el area y perimetro de un triangulo cualquiera") # quiero que imprima el siguento texto 
+base= int(input("ingresa el valor de base: ")) # le asigne una variable un valor tipo entero y un texto para que me ingrese un una cantidad al momento de solicitarlo, este es la base 
+lado1= int(input("ingresa el lado1: ")) # sera el unos de los lados del triangulo
+lado2= int(input("ingresa el lado2: ")) #otro lado 
+altura= int(input("ingresa la altura:" )) #la altura del triangulo
+
+perimetro=lado1+lado2+base # para calacular el perimetro asigno una variable llamado perimetro, despues pongo la formula de dicha figura 
+
+print("el perimetro es: ",perimetro) # por ultimo imprimo el resultado poniendo un texto y cerrando comillas pongo una coma pa luego poner el nombre de la variable
+
+area= (base*altura) /2 #hago lo mismo pero ahora con la formuña del area
+
+print("El area de es: ",area) #respectivamente imprimo el area 
+
+# dato despues de una semana de que empeze el reto eh mejorado mucho a el primer dia, casi no tuve que consultar el ejercicio ("solamente para checar u error que tuve en la sintaxis") realizado en clase  me tarde solamente 14 min en terminar, el primer dia fueron 27 min solo para imprimir hola mundo, se me olvido comentarlo en la otra semana pero si le muevo tengo miedo de que se me actualize la fecha al igual que me se me a olvidado comentar los otros dias pero tratere de que no se me olvide en esta semana creo que hasta aqui le dejo yam boy amimir...
