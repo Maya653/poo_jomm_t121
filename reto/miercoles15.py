50f3b538a90457e914c69f94ae36a6e76a2f3229
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-02-16 03:58:01 +0000

    calcular las formulas de un cuadrado

---
+++ reto/miercoles15.py
@@ -0,0 +1,19 @@
+""""
+    Nombre: Johan Obed Maya Morales
+    Fecha:9-Feb-2023
+    Descripción: calcular area y perimetro de un cuadrado
+""""" 
+print("calcuclar la formulas de un cuadadro") # qye me imorima en la pantalla lo que se realizara
+
+lado1=int(input("ingresa el lado1: ")) # creo variables para cada lado, de tipo entero y que ingrese un valor cualquiera
+lado2=int(input("ingresa ellado2: "))
+lado3=int(input("ingresa el lado3: "))
+lado4=int(input("ingresa el lado4: "))
+
+perimetro= lado1 + lado2+ lado3+ lado4 #pongo la formula del cuadtado creando una variable llamado perimetro
+print("el resultado del perimtro es: ", perimetro) # que me imprima el elresultado
+
+area= lado1 * lado2
+print("el resultado de area es: ", area) # de igual manera solo que esta me umprima el resultado del area
+
+#dato ni siquiera vi de paso el otro programa solo entre al replit y cree lun nuevo file y ya me puse a hacer el ejercicio 
