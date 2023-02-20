d3c96ee2df6fb78a1d839c0e0625943e273ca15b
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-02-20 05:13:19 +0000

    tem de grillo

---
+++ reto/domingo19.py
@@ -0,0 +1,14 @@
+"""""
+Nombre: Johan Obed Maya Morales
+Fecha: 19-feb-23
+Descripcion: calcular la temperatura  de un grillo
+"""""
+
+ne=int(input("ingrese el numero: "))
+tem= ne/4+40
+
+if ne > 0:
+    print("su temperatura es: ",tem)
+else:
+    print("No emite sonido")
+    
