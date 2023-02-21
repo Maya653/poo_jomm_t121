d14b9f8ffddbf3a9c9eb9ba34e3b76b44b138e58
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-02-21 04:42:27 +0000

    una gasolinera

---
+++ reto/lunes20.py
@@ -0,0 +1,13 @@
+""""Nombre : Johan Obed Maya morales
+    Fecha: 20-feb-2023
+    descripcion: Gasolinera
+"""""
+print("Calcule los galones a litro y el precio total de la gasolina: ")
+
+galones= float(input("ingresa la cantidad de galones:"))
+litros= galones *3.78
+print("Son",galones,"galones a litros son: ",litros)
+
+litros1= float(input("ingrese la cantidad en litros: "))
+precio= litros * 21*30
+print("Total del precio: ",precio)
