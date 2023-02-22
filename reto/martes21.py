5392fa5e61aba88a01f4b3e4ed7c472a4cca4f06
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-02-22 02:38:16 +0000

    reto del martes21 de un dia en segundos

---
+++ reto/martes21.py
@@ -0,0 +1,11 @@
+""""Nombre: Johan Obed Maya Morales
+    Fecha: 21-feb-2023
+    Descripcion: nunmero de segundos que hay en determinado numero de dias 
+"""""
+
+dias = int(input("Ingresa el numero de dias: ")) # se creo una variable que ingrese un valor de tipo entero 
+segundos = (dias*24*60*60) # se creo una variable que haga una operacion para calcular los segundos dependiendo el numero de dias que se ingrese
+print("El dia ",dias, "en segundos son: ", segundos) #imprimira los dias que se ingreso y la cantidad de segundos
+
+
+    
