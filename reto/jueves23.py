58289883deaf58eb8e415e555a094431a5ddeba7
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-02-24 05:45:09 +0000

    jueves 23/feb

---
+++ reto/jueves23.py
@@ -0,0 +1,9 @@
+"""""
+NOmbre: JOhan Obed Maya Morales
+Fecha: 23-Feb-2023
+Descripcion: 
+"""""
+desvelar =input("ingresa lo que sientes: ")
+porque= input("Porque:")
+por_que_elegi_esta_carrera = desvelar + porque
+print("estoy",por_que_elegi_esta_carrera)
