85da61f4033e1847f0d7fd800b60c876ca74dd8a
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-02-26 04:32:48 +0000

    reto incum

---
+++ reto/sabado25.py
@@ -0,0 +1,15 @@
+""""
+nombre: Johan Obed Maya Morales
+Fecha: 25 - feb-2023
+descripcion: 
+"""""
+a = int(input("ingrese a: "))
+b = int(input("ingrese b: "))
+c= int(input("ingrese c: "))
+
+if  a>b:
+    print("los numeros estan Ordenados: ")
+elif a<b:
+    print("estan desordenados:")
+elif a==c:
+    print("son iguales. ")
